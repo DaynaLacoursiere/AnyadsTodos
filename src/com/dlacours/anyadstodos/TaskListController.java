41be216326ec8217d4e398aa6a559bf073cb0f76
DaynaLacoursiere <dlacours@ualberta.ca>
2014-09-18 20:07:55 -0600

    Missed these in the last commit

---
+++ src/com/dlacours/anyadstodos/TaskListController.java
@@ -0,0 +1,21 @@
+package com.dlacours.anyadstodos;
+
+
+public class TaskListController {
+	private static TaskList taskList = null;
+	
+	static public TaskList getTaskList() {
+		if (taskList == null){
+			taskList = new TaskList();
+		}
+		return taskList;
+	}
+	
+	public void addTask(Task task){
+		getTaskList().addTask(task);
+	}
+	
+	public void deleteTask(Task task){
+		getTaskList().removeTask(task);
+	}
+}
